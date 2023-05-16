aa54b7e08998cdda6c653aee09ac6226bafa47dc
Andres Arutro Rodriguez Calderon <andrescalderonn1995@gmail.com>
2023-05-16 22:22:21 +0300

    rental service retry inventory service

--- rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClientFallback.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClientFallback.java
@@ -1,21 +1,9 @@
 package com.kodlamaio.rentalservice.api.clients;
 
 import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
-import feign.RetryableException;
-import io.github.resilience4j.retry.annotation.Retry;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.openfeign.FallbackFactory;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
