cde1ae65283cc6736e246960d4670981020ad571
andresrodriguez55 <19671994kurtcobain19671994@gmail.com>
2023-05-20 10:56:03 +0300

    maintenance service added

---
+++ maintenance-service/src/main/java/com/kodlamaio/maintenanceservice/MaintenanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kodlamaio.maintenanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceServiceApplication.class, args);
+	}
+
+}
---
+++ maintenance-service/src/test/java/com/kodlamaio/maintenanceservice/MaintenanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodlamaio.maintenanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
