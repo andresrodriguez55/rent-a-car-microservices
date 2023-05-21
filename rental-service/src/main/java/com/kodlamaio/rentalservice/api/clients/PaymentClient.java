64d5da02a812bd546c32395e714355eb86bf79fd
andresrodriguez55 <19671994kurtcobain19671994@gmail.com>
2023-05-21 11:49:30 +0300

    payment service updated

--- rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/PaymentClient.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/PaymentClient.java
@@ -4,12 +4,13 @@ import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
 import com.kodlamaio.commonpackage.utils.dto.CreateRentalPaymentRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "payment-service",
         fallback = PaymentClientFallback.class)
 public interface PaymentClient
 {
     @PostMapping(value = "/api/payments/process-rental-payment", consumes = "application/x-www-form-urlencoded")
-     ClientResponse processRentalPayment(CreateRentalPaymentRequest request)
+     ClientResponse processRentalPayment(@RequestBody CreateRentalPaymentRequest request)
             throws InterruptedException;
 }
